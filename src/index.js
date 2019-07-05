cda34e12dcbab4e4fd5a228bf56e0a3f416f0647
Philip Morris <philip.morris@intersection.com>
2019-07-05 23:30:27 +0100

    add response object to handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports.handler = async () => {
-  return {'message': 'Hello from API'};
+module.exports.handler = (event, context, callback) => {
+  callback(null, {
+    statusCode: 200,
+    headers: {'Access-Control-Allow-Origin': '*'},
+    body: JSON.stringify({'message': 'Hello from API'})
+  });
 };
