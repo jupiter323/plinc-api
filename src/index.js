c435ef2e58cabd9d096559dd566de4e81aefa6e3
Philip Morris <philip.morris@intersection.com>
2019-07-06 20:52:59 +0100

    add Access-Control-Allow-Credentials

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
-    headers: {'Access-Control-Allow-Origin': '*'},
+    headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Methods': '*', 'Access-Control-Allow-Headers': '*'},
     body: JSON.stringify({'message': 'Hello from API'})
   });
 };
