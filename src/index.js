18dc41436807f5c3f66efb025835136dda15eba1
Philip Morris <philip.morris@intersection.com>
2019-07-08 16:27:45 +0100

    e2e test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ module.exports.handler = (event, context, callback) => {
   callback(null, {
     statusCode: 200,
     headers: {'Access-Control-Allow-Origin': '*', 'Access-Control-Allow-Credentials': true, 'Access-Control-Allow-Methods': '*', 'Access-Control-Allow-Headers': '*'},
-    body: JSON.stringify({'message': 'Hello from API'})
+    body: JSON.stringify({'message': 'Hello from API!!!'})
   });
 };
