b680e40d0640a98a1e55576ee63eddd5d1758b3a
Philip Morris <philip.morris@intersection.com>
2019-07-26 17:18:29 +0100

    test getchinh a list

--- src/index.integration.test.js
+++ src/index.integration.test.js
@@ -19,7 +19,7 @@ test('UnAuthentication', (t) => {
 });
 
 test('Authenticated', (t) => {
-  t.plan(4);
+  t.plan(9);
 
   cognito.generateUser().then(({user, token}) => {
     axios({
@@ -27,15 +27,27 @@ test('Authenticated', (t) => {
       url: `${API_URL}/lists`,
       headers: {'Authorization': `Bearer ${token}`, 'accept': 'application/json' },
       data: {
-        "title": "Title",
-        "description": "Description",
-        "category": "Category",
+        "title": "Test",
+        "description": "Integration Test List",
+        "category": "Integration",
         "public": true
       }
     }).then(res => {
       t.equal(res.status, 201, 'should be 201');
       t.equal(res.statusText, 'Created', 'should be Created');
       t.ok(res.data.listId, 'has list id');
+
+      axios({
+        method: 'GET',
+        url: `${API_URL}/lists/${user.username}/${res.data.listId}`,
+        headers: {'Authorization': `Bearer ${token}`, 'accept': 'application/json' }
+      }).then(res => {
+        t.equal(res.status, 200, 'should be 200');
+        t.equal(res.data.Title, 'Test');
+        t.equal(res.data.Description, 'Integration Test List');
+        t.equal(res.data.Category, 'Integration');
+        t.equal(res.data.Public, true);
+      });
 
       cognito.deleteUser({ Username: user.username }).then(() => {
         t.pass('DONE!');
