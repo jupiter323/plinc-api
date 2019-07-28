545fef4e0865df9673888e9e2cc304ab1fe4e431
Philip Morris <philip.morris@intersection.com>
2019-07-28 10:24:05 +0100

    add correct bundle spec

--- swagger/index.js
+++ swagger/index.js
@@ -1,9 +1,9 @@
 require('../node_modules/swagger-ui/dist/swagger-ui.css');
 
 import SwaggerUI from 'swagger-ui';
-import Spec from './spec.json';
+import Spec from './bundle.spec.json';
 
 SwaggerUI({
   spec: Spec,
-  dom_id: '#app'
+  dom_id: '#app',
 });
