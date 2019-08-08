59368dfc07fbc979e679b2cff03582cd52185f64
Philip Morris <philip.morris@intersection.com>
2019-08-08 15:10:36 +0100

    add items

---
+++ src/handlers/items.js
@@ -0,0 +1,62 @@
+const decode = require('jwt-claims');
+
+const Items = require('../repositories/items');
+
+const lists = new Items(process.env.ITEMS_TABLE_NAME);
+
+const headers = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Credentials': true,
+  'Access-Control-Allow-Methods': '*',
+  'Access-Control-Allow-Headers': '*',
+};
+
+module.exports.create = (event, context, callback) => {
+  if (event.body !== null && event.body !== undefined) {
+    const claims = decode(event.headers.Authorization.replace('Bearer ', ''));
+
+    const body = JSON.parse(event.body);
+
+    body.possessor = claims['cognito:username'];
+
+    lists
+      .create(body)
+      .then((response) => {
+        callback(null, {
+          statusCode: 201,
+          headers,
+          body: JSON.stringify(response),
+        });
+      })
+      .catch((err) => {
+        callback(err, {
+          statusCode: 500,
+          headers,
+          body: JSON.stringify(err),
+        });
+      });
+  }
+};
+
+module.exports.getAll = (event, context, callback) => {
+  const params = {
+    listId: event.pathParameters.listId,
+  };
+
+  lists
+    .getAll(params)
+    .then((response) => {
+      callback(null, {
+        statusCode: 200,
+        headers,
+        body: JSON.stringify(response),
+      });
+    })
+    .catch((err) => {
+      callback(err, {
+        statusCode: 500,
+        headers,
+        body: JSON.stringify(err),
+      });
+    });
+};
---
+++ src/repositories/items.js
@@ -0,0 +1,66 @@
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+const utils = require('../dynamo.utils');
+
+const schema = {
+  ListId: 'S',
+  ItemId: 'S',
+  Description: 'S',
+};
+
+const unpack = utils.unpack(schema);
+
+class Items {
+  constructor(tableName) {
+    this.tableName = tableName;
+    this.dynamodb = new AWS.DynamoDB();
+  }
+
+  item(props) {
+    return {
+      TableName: this.tableName,
+      Item: utils.pack(schema, props),
+      ReturnConsumedCapacity: 'TOTAL',
+    };
+  }
+
+  create(params) {
+    const itemId = uuid.v4();
+    return new Promise((resolve, reject) => {
+      this.dynamodb
+        .putItem(this.item({ ...params, itemId }))
+        .promise()
+        .then(() => {
+          resolve({ itemId });
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+
+  getAll(params) {
+    const query = {
+      TableName: this.tableName,
+      KeyConditionExpression: 'ListId = :listId',
+      ExpressionAttributeValues: {
+        ':listId': { S: params.listId },
+      },
+      ReturnConsumedCapacity: 'TOTAL',
+    };
+
+    return new Promise((resolve, reject) => {
+      this.dynamodb
+        .query(query)
+        .promise()
+        .then((response) => {
+          resolve(response.Items.map(unpack));
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
+}
+
+module.exports = Items;
