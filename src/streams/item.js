d47c187ea17d6fab061d39831ebc5b2488b2be38
Philip Morris <philip.morris@intersection.com>
2019-08-08 21:23:22 +0100

    log dynamo object

--- src/streams/item.js
+++ src/streams/item.js
@@ -1,5 +1,7 @@
 module.exports.handler = function(event, context, callback) {
   console.log('receiving event');
   console.log(event);
+  console.log('dynamodb');
+  console.log(JSON.stringify(event.dynamodb));
   callback(null, 'done');
 };
