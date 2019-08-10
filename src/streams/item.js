0f43a756b57c4bffa659ebfa279a826841cfa0c8
Philip Morris <philip.morris@intersection.com>
2019-08-10 20:41:12 +0100

    logging

--- src/streams/item.js
+++ src/streams/item.js
@@ -8,6 +8,9 @@ module.exports.handler = (event, context, callback) => {
       const listId = record.dynamodb.Keys.ListId.S;
       const possessor = record.dynamodb.NewImage.Possessor.S;
 
+      console.log('KEYS', record.dynamodb.Keys);
+      console.log('NEW IMAGE:', record.dynamodb.NewImage);
+
       await lists.increment({ possessor, id: listId });
     }
   });
