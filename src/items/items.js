128102e26718d1d6b675e48d00de795bd0d3289f
Nural <jupiter.fierce.31@gmail.com>
2020-03-23 22:06:24 +0200

    origin return itmeid

--- src/items/items.js
+++ src/items/items.js
@@ -30,7 +30,7 @@ class Items {
     const itemId = uuid.v4();
     const item = this.item({ itemId, ...params });
     await this.dynamodb.put(item);
-    return { itemId: params.itemId || itemId };
+    return { itemId };
   }
 
   async getAll(params) {
